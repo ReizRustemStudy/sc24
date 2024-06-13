ec4f5e2ca655faf7d91df14af52337ccb8aa6b1a
Rustem <rustem.reizov.developer@gmail.com>
2024-06-13 16:13:09 +0300

    create DefaultProductService into manager-app

---
+++ manager-app/src/main/java/com/reizovrustem/study/manager/service/DefaultProductService.java
@@ -0,0 +1,51 @@
+package com.reizovrustem.study.manager.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.reizovrustem.study.manager.entity.Product;
+import com.reizovrustem.study.manager.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DefaultProductService implements ProductService {
+
+	private final ProductRepository productRepository;
+
+	@Override
+	public List<Product> findAllProducts() {
+		return productRepository.findAll();
+	}
+
+	@Override
+	public Product createProduct(String title, String details) {
+		return productRepository.save(new Product(null, title, details));
+	}
+
+	@Override
+	public Optional<Product> findProduct(int productId) {
+		return productRepository.findById(productId);
+	}
+
+	@Override
+	public void updateProduct(Integer id, String title, String details) {
+		productRepository.findById(id)
+			.ifPresentOrElse(product -> {
+				product.setTitle(title);
+				product.setDetails(details);
+			}, () -> {
+				throw new NoSuchElementException();
+			});
+	}
+
+	@Override
+	public void deleteProduct(Integer id) {
+		productRepository.deleteById(id);
+		
+	}
+}
