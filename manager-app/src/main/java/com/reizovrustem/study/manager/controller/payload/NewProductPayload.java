8a3606a02de772b47f1a557e482502a377b70209
Rustem <rustem.reizov.developer@gmail.com>
2024-06-13 16:42:18 +0300

    edit NewProductsPaylod into manager-app creates validation

--- manager-app/src/main/java/com/reizovrustem/study/manager/controller/payload/NewProductPayload.java
+++ manager-app/src/main/java/com/reizovrustem/study/manager/controller/payload/NewProductPayload.java
@@ -1,5 +1,14 @@
 package com.reizovrustem.study.manager.controller.payload;
 
-public record NewProductPayload(String title, String details) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record NewProductPayload(
+		
+		@NotNull
+		@Size(min = 3, max = 5)
+		String title, 
+		@Size(max = 1000)
+		String details) {
 
 }
