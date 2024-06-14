16aa9d7034c76f1d78e37caf5bd771f5c8090799
Rustem <rustem.reizov.developer@gmail.com>
2024-06-14 17:08:45 +0300

    update ProductController into manager-app create validation

--- manager-app/src/main/java/com/reizovrustem/study/manager/controller/ProductController.java
+++ manager-app/src/main/java/com/reizovrustem/study/manager/controller/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,6 +21,7 @@ import com.reizovrustem.study.manager.entity.Product;
 import com.reizovrustem.study.manager.service.ProductService;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -47,9 +50,18 @@ public class ProductController {
 	}
 	
 	@PostMapping("edit")
-	public String updateProduct(@ModelAttribute("product") Product product, UpdateProductPayload payload) {
-		productService.updateProduct(product.getId(), payload.title(), payload.details());
-		return "redirect:/catalogue/products/%d".formatted(product.getId());
+	public String updateProduct(@ModelAttribute(value ="product", binding = false) Product product,
+			@Valid UpdateProductPayload payload, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("payload", payload);
+			model.addAttribute("errors", bindingResult.getAllErrors().stream()
+					.map(ObjectError::getDefaultMessage)
+					.toList());
+			return "catalogue/products/edit";
+		} else {
+			productService.updateProduct(product.getId(), payload.title(), payload.details());
+			return "redirect:/catalogue/products/%d".formatted(product.getId());
+		}
 	}
 	
 	@PostMapping("delete")
