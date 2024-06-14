d5fcd34e16f3705adcbfeaeb5eacd67987a41933
Rustem <rustem.reizov.developer@gmail.com>
2024-06-14 17:09:27 +0300

    update ProductsController into manager-app create validation

--- manager-app/src/main/java/com/reizovrustem/study/manager/controller/ProductsController.java
+++ manager-app/src/main/java/com/reizovrustem/study/manager/controller/ProductsController.java
@@ -2,6 +2,8 @@ package com.reizovrustem.study.manager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import com.reizovrustem.study.manager.controller.payload.NewProductPayload;
 import com.reizovrustem.study.manager.entity.Product;
 import com.reizovrustem.study.manager.service.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -32,8 +35,17 @@ public class ProductsController {
 	}
 	
 	@PostMapping("create")
-	public String createProduct(NewProductPayload payload) {
-		Product product = productService.createProduct(payload.title(), payload.details());
-		return "redirect:/catalogue/products/list";
+	public String createProduct(@Valid NewProductPayload payload, BindingResult bindingResult,
+			Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("payload", payload);
+			model.addAttribute("errors", bindingResult.getAllErrors().stream()
+					.map(ObjectError::getDefaultMessage)
+					.toList());
+			return "catalogue/products/new_product";
+		} else {
+			Product product = productService.createProduct(payload.title(), payload.details());
+			return "redirect:/catalogue/products/%d".formatted(product.getId());
+		}
 	}	
 }
